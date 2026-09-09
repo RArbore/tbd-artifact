636d2bbab6161049770fa34279bf12f75a0f9d1c
Russel Arbore <russel.jma@gmail.com>
2026-09-09 15:24:01 -0700

    Basic set of rewrite rules

--- eqsat-ai/build.rs
+++ eqsat-ai/build.rs
@@ -1,3 +1,12 @@
+use std::env::current_dir;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
     lalrpop::process_root().unwrap();
+
+    // Compile rewrite rules into Rust code that implements the required relational e-matching.
+    let mut file = File::open(current_dir().unwrap().join("src/rw.rules")).unwrap();
+    let mut contents = "".to_string();
+    file.read_to_string(&mut contents).unwrap();
 }
