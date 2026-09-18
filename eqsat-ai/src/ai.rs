d2b6dfc020488f6580816d478cc6bde3642768eb
Russel Arbore <russel.jma@gmail.com>
2026-09-18 16:30:39 -0700

    Separate delta into delta_new and delta_union

--- eqsat-ai/src/ai.rs
+++ eqsat-ai/src/ai.rs
@@ -240,7 +240,10 @@ impl<'a> AIContext<'a> {
 
     fn ensure_analyzed(&mut self, version: SSABlockId) {
         match self.versions.get_mut(&version).unwrap() {
-            VersionState::Mutable(version) => self.saturator.saturate(version),
+            VersionState::Mutable(version) => {
+                self.saturator.saturate(version);
+                assert!(self.saturator.is_delta_new_empty());
+            }
             VersionState::Immutable(_) => {}
         }
     }
